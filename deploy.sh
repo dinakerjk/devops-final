0aa57586066365993f8e23954794fdef5eb2b9cf
dinakerjk <127740119+dinakerjk@users.noreply.github.com>
2025-04-05 09:25:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hyyy
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u surethan37 -p 55665566@S37
-    docker tag test surethan37/task
+    docker login  -p 55665566@S37
+    docker tag test/task
     docker push surethan37/task
     
